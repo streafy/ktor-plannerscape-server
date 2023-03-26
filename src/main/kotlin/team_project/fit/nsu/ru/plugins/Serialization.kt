d56afece3781a6082eccb6ad7a48c71ddf659df2
Ivan <Streafyhs@gmail.com>
2023-03-27 04:04:18 +0700

    added post request support to send commands list to server

--- src/main/kotlin/team_project/fit/nsu/ru/plugins/Serialization.kt
+++ src/main/kotlin/team_project/fit/nsu/ru/plugins/Serialization.kt
@@ -1,9 +1,7 @@
 package team_project.fit.nsu.ru.plugins
 
 import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.response.*
 import io.ktor.server.application.*
-import io.ktor.server.routing.*
 import io.ktor.serialization.gson.*
 
 fun Application.configureSerialization() {
--- src/main/kotlin/team_project/fit/nsu/ru/routes/CommandRoutes.kt
+++ src/main/kotlin/team_project/fit/nsu/ru/routes/CommandRoutes.kt
@@ -1,10 +1,12 @@
 package team_project.fit.nsu.ru.routes
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import team_project.fit.nsu.ru.dao.dao
+import team_project.fit.nsu.ru.models.Command
 
 fun Route.commandRouting() {
     route("/commands") {
@@ -12,7 +14,9 @@ fun Route.commandRouting() {
             call.respond(dao.allCommands())
         }
         post {
-            val formParameters = call.receiveParameters()
+            val commands = call.receive<List<Command>>()
+            commands.forEach { dao.addNewCommand(it) }
+            call.respondText("Commands stored correctly", status = HttpStatusCode.Created)
         }
     }
 }
