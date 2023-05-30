1bfc133b0cbd8c4fdf8ea313c4e5e56d7e8baf42
Ivan <Streafyhs@gmail.com>
2023-05-31 02:11:47 +0700

    trash

--- src/main/kotlin/team_project/fit/nsu/ru/routes/CommandRoutes.kt
+++ src/main/kotlin/team_project/fit/nsu/ru/routes/CommandRoutes.kt
@@ -1,6 +1,5 @@
 package team_project.fit.nsu.ru.routes
 
-import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
@@ -11,17 +10,28 @@ import team_project.fit.nsu.ru.models.Command
 fun Route.commandRouting() {
     route("/commands") {
         get {
-            call.respond(dao.allCommands())
+            println("||||   some client asked for all commands")
+            val allCommands: List<Command> = dao.allCommands()
+            call.respond(allCommands)
+            println("||||   returning $allCommands to this client")
         }
         get("/{id}") {
             val id: Int? = call.parameters["id"]?.toInt()
-            println("$id <-- id here")
-            call.respond(dao.neededCommands(id ?: 0))
+            println("||||   some client asked for all commands from $id")
+            val neededCommands: List<Command> = dao.neededCommands(id ?: 0)
+            call.respond(neededCommands)
+            println("||||   returning $neededCommands to this client")
         }
         post {
+            val initialCount: Int = dao.allCommands().size
             val commands = call.receive<List<Command>>()
             commands.forEach { dao.addNewCommand(it) }
-            call.respondText("Commands stored correctly", status = HttpStatusCode.Created)
+
+            println("||||   added(${commands.size}) $commands\n")
+            val allCommands: List<Command> = dao.allCommands()
+            val afterAdditionCount: Int = allCommands.size
+            println("||||   current storage(${allCommands.size}): $allCommands")
+            call.respond((initialCount + 1..afterAdditionCount).toList())
         }
     }
 }
